dd6c5ff8b3c093dd5783e6be48124b20eaf04295
Josemi Chaves <josemichaves@gmail.com>
2021-03-20 03:23:20 +0100

    Fixed start positions and a bug with while condition

--- workspaces/frontend/src/components/mapView/mapView.tsx
+++ workspaces/frontend/src/components/mapView/mapView.tsx
@@ -7,13 +7,13 @@ import './mapViewStyles.scss';
 
 const MapView = () => {
   const startPos = {
-    lat: 40.0,
-    lng: 1.0,
+    lat: 39.539705,
+    lng: 2.740096,
   };
 
   const finalPos = {
-    lat: 40.1,
-    lng: 1.100001,
+    lat: 39.57184,
+    lng: 2.643305,
   };
 
   const [actualPos, setActualPos] = useState(startPos);
@@ -21,15 +21,15 @@ const MapView = () => {
   //Generate array first, and then iterate in this array
 
   const changeLocation = async () => {
-    while (actualPos.lat && actualPos.lng < finalPos.lng && finalPos.lat) {
+    while (actualPos.lat && actualPos.lng > finalPos.lng && finalPos.lat) {
       await new Promise((res) => {
         console.log(actualPos);
         setTimeout(() => res(''), 500);
       });
       setActualPos((prevPos) => {
         return {
-          lat: prevPos.lat + 0.01,
-          lng: prevPos.lng + 0.01,
+          lat: prevPos.lat + 0.0032135,
+          lng: prevPos.lng - 0.0096791,
         };
       });
     }
@@ -47,8 +47,8 @@ const MapView = () => {
               <div>
                 <MapContainer
                   center={latLng(actualPos)}
-                  zoom={8}
-                  scrollWheelZoom={false}>
+                  zoom={13}
+                  scrollWheelZoom={true}>
                   <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
