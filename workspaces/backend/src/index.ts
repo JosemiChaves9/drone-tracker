9490295e12befa7b464c10f1125c1b49c53184a7
Josemi Chaves <josemichaves@gmail.com>
2021-04-09 16:54:46 +0200

    fixed a miss import

--- workspaces/backend/src/index.ts
+++ workspaces/backend/src/index.ts
@@ -1,10 +1,8 @@
 import Express from 'express';
-import { bases } from '../mockData';
 import { connectDb } from './dbConnection';
 
 const app = Express();
 const PORT = process.env.PORT || 4000;
-app.get('/', (req, res) => res.send(bases));
 
 app.listen(PORT, () =>
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
