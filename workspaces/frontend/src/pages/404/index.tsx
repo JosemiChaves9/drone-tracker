404e18579addf1ffdca501e5aeee56b2fda8517f
Josemi Chaves <josemichaves@gmail.com>
2021-04-20 16:38:00 +0200

    React router working, integrated links, and 404 page

---
+++ workspaces/frontend/src/pages/404/index.tsx
@@ -0,0 +1,40 @@
+import './index.scss';
+
+export const Page404 = () => {
+  return (
+    <div id='layoutError'>
+      <div id='layoutError_content'>
+        <main>
+          <div className='container'>
+            <div className='row justify-content-center'>
+              <div className='col-lg-6'>
+                <div className='text-center mt-4'>
+                  <img
+                    className='mb-4 img-error'
+                    src='error-404-monochrome.svg'
+                  />
+                  <p className='lead'>
+                    This requested URL was not found on this server.
+                  </p>
+                  <a href='index.html'>
+                    <i className='fas fa-arrow-left mr-1'></i>
+                    Return to Dashboard
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+      <div id='layoutError_footer'>
+        <footer className='py-4 bg-light mt-auto'>
+          <div className='container-fluid'>
+            <div className='small'>
+              <div className='text-muted'>Copyright &copy; DroneTracker</div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
