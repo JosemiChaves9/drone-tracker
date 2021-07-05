ffd211e81345c7ea7d952671bcd3400ece593529
Josemi Chaves <josemichaves@gmail.com>
2021-07-05 11:47:37 +0200

    deploy with heroku

--- workspaces/frontend/src/pages/home/index.tsx
+++ workspaces/frontend/src/pages/home/index.tsx
@@ -16,6 +16,7 @@ export const Home = () => {
   });
 
   useEffect(() => {
+    console.log(process.env);
     const ws = new WebSocket(
       `${EnviromentVariables.getWebSocketAddressAndPort()}/1MZ50`
     );
